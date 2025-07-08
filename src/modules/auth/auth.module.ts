ebb112ba5c94fe4a99422badcad7a917605e6192
An Nguyen <andqnguyen.work@gmail.com>
2025-07-08 14:45:13 +0700

    Add JWT authentication

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from '@/modules/user/user.schema';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({
+      secret: 'secretKey123', // => nên để vào ENV
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [JwtModule],
+})
+export class AuthModule {}
---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from '@/modules/user/user.schema';
+import { JwtService } from '@nestjs/jwt';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private jwtService: JwtService,
+    @InjectModel(User.name) private userModel: Model<User>,
+  ) {}
+
+  async login(username: string) {
+    let user = await this.userModel.findOne({ username });
+    if (!user) {
+      user = await this.userModel.create({ username });
+    }
+
+    const payload = { sub: user._id, username: user.username };
+    const token = this.jwtService.sign(payload);
+
+    return {
+      access_token: token,
+      user: {
+        id: user._id,
+        username: user.username,
+      },
+    };
+  }
+
+  async verifyToken(token: string) {
+    return this.jwtService.verify(token);
+  }
+}
