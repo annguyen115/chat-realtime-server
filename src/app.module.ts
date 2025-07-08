4ce827736aa6bf2a3de597a26b3c197f46574678
An Nguyen <andqnguyen.work@gmail.com>
2025-07-08 14:28:05 +0700

    Add User & Message schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ChatGateway } from '@/modules/chat/chat.gateway';
+import { UserModule } from '@/modules/user/user.module';
+import { MessageModule } from '@/modules/message/message.module';
+import { User, UserSchema } from '@/modules/user/user.schema';
+import { Message, MessageSchema } from '@/modules/message/message.schema';
+
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/chat-app'),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Message.name, schema: MessageSchema },
+    ]),
+    UserModule,
+    MessageModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ChatGateway],
 })
 export class AppModule {}
---
+++ src/modules/message/message.schema.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+import { User } from '@/modules/user/user.schema';
+
+@Schema()
+export class Message extends Document {
+  @Prop({ required: true })
+  content: string;
+
+  @Prop({ type: Date, default: Date.now })
+  timestamp: Date;
+
+  @Prop({ type: Types.ObjectId, ref: 'User' })
+  user: User;
+}
+
+export const MessageSchema = SchemaFactory.createForClass(Message);
